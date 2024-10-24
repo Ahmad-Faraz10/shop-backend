1a124e3c6280c259bf2c5f0935814f3779d0de70
Ahmad Faraz <2020ahmadfaraz@gmail.com>
2024-10-24 21:36:30 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const db_url = process.env.DATABASE;
+const db_url ="mongodb+srv://root:root@cluster0.vkunc9q.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
   .connect(db_url, {
